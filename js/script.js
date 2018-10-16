2fbfcf5aa1a0f1c434bcfaa8154556de5e18d2fa
TessKi <kinnte@gmail.com>
2018-10-16 09:38:24 +0100

    createSquares function refactored

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
 const container = document.querySelector('#container');
 const buttons = document.querySelectorAll('.button');
-let currentPlayer = true;
 
-const squareIds = [];
+const squareIds = []; // where all the square objects go
 const letters = ['a', 'b', 'c', 'd'];
 let currentLetterIndex = 0;
-let currentNumber = 0;
+let squareNumberId = 0;
 const board = {
   a: 1,
   b: 1,
   c: 1,
   d: 1
 };
+let currentPlayer = true;
+const player1tokens = [];
+const player2tokens = [];
 
-function createSquares(numberOfSquares) {
+// squareLetters = Columns (numberOfColumns)
+// squareNumbers = Rows (numberOfRows)
+
+function createSquares(numberOfRows, numberOfColumns) {
+  const numberOfSquares = numberOfRows * numberOfColumns;
   for (let i = 1; i <= numberOfSquares; i++) {
-    currentNumber ++;
-    if (currentNumber > 4) {
-      currentNumber = 1;
+    squareNumberId ++;
+    if (squareNumberId > numberOfRows) {
+      squareNumberId = 1;
     }
-    const letter = letters[currentLetterIndex];
+    const squareLetterId = letters[currentLetterIndex];
     const newObject = {
-      name: letter + currentNumber,
+      name: squareLetterId + squareNumberId,
       isOccupied: null,
       domElement: document.createElement('div')
     };
@@ -29,17 +35,14 @@ function createSquares(numberOfSquares) {
     container.appendChild(newObject.domElement);
     newObject.domElement.setAttribute('id', newObject.name);
     newObject.domElement.innerHTML = newObject.name;
-    if(i % 4 === 0) {
+    if(i % numberOfColumns === 0) {
       currentLetterIndex ++;
     }
     squareIds.push(newObject);
   }
 }
 
-createSquares(16);
-
-const player1tokens = [];
-const player2tokens = [];
+createSquares(6, 6);
 
 // event listeners on the buttons and player interaction
 for (let i = 0; i < buttons.length; i++)
@@ -59,6 +62,7 @@ for (let i = 0; i < buttons.length; i++)
       // console.log('isOccupied is', squareIds.isOccupied);
       console.log('player 1 has', player1tokens);
       currentPlayer = !currentPlayer;
+
     } else {
       targetDiv.classList.add('square-clicked-player2');
       squareIds.isOccupied = 'player2';
@@ -69,3 +73,5 @@ for (let i = 0; i < buttons.length; i++)
       currentPlayer = !currentPlayer;
     }
   });
+
+// WIN LOGIC, whaaaaa!
